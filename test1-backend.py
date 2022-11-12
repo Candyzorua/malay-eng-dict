1fae7fc62ede5f37a14bddf607e0065bcef63c42
Candyzorua <jiaying.ong2@gmail.com>
2022-11-12 13:45:35 -0800

    Implemented frontend data sending to backend

--- test1-backend.py
+++ test1-backend.py
@@ -1,7 +1,7 @@
 import json
 
 import flask
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -11,12 +11,24 @@ CORS(app)
 def hello():
     return "Hello, World!"
 
-@app.route('/words', methods=["GET"])
-def users():
+@app.route('/words', methods=["GET", "POST"])
+def words():
     print("words endpoint reached...")
-    with open("malay-eng-dict.json", "r") as f:
-        data = json.load(f)
-        return flask.jsonify(data)
+
+    if request.method == "GET":
+        with open("malay-eng-dict.json", "r") as f:
+            data = json.load(f)
+            return flask.jsonify(data)
+
+    if request.method == "POST":
+        received_data = request.get_json()
+        print(f"received data: {received_data}")
+        message = received_data['data']
+        return_data = {
+            "status": "success",
+            "message": f"received: {message}"
+        }
+        return flask.Response(response=json.dumps(return_data), status=201)
 
 if __name__ == "__main__":
     app.run("localhost", 6969)

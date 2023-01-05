93f6b068fddd842a70cc73a51f59310d866baa7b
Candyzorua <jiaying.ong2@gmail.com>
2023-01-04 16:16:53 -0800

    Output now does not strip punctuation and case

--- get_translation_function.py
+++ get_translation_function.py
@@ -1,32 +1,30 @@
 import re
 import json
-example_input = "aku suka kamu!"
+example_input = "Aku Suka"
 
 
 def cleanwords(rawWordString: str) -> list[str]:
     noPunctWords = []
     rawWords = rawWordString.split(" ")
     for w in rawWords:
-        noPunctString = re.sub(r'[^\w\s]', '', w)
+        noPunctString = re.sub(r'[^\w\s]', '', w).lower()
         noPunctWords.append(noPunctString)
     return noPunctWords
 
-
-assert(cleanwords(example_input)) == ['aku', 'suka', 'kamu']
-
+print(example_input.split(" "))
+print(cleanwords(example_input))
 
 def findtranslations(rawWordString: str) -> dict:
-    malayWords = cleanwords(rawWordString)
+    rawMalayWords = rawWordString.split(" ")
+    cleanMalayWords = cleanwords(rawWordString)
     translationsDict = {}
     with open("malay_eng_dict.json", "r") as f:
         malayDict = json.load(f)
-        for w in malayWords:
-            if w in malayDict:
-                translationsDict[w] = malayDict[w]
+        for i in range(0, (len(cleanMalayWords))):
+            if cleanMalayWords[i] in malayDict:
+                translation = malayDict[cleanMalayWords[i]]
             else:
-                translationsDict[w] = "n/a"
+                translation = "n/a"
+            translationsDict[rawMalayWords[i]] = translation
     return translationsDict
 
-
-assert(findtranslations(example_input)) == {
-    'aku': 'me; I', 'suka': 'like; enjoy', 'kamu': 'you; your'}

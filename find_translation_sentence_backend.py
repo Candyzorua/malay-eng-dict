c15a8b78f2693e158d5633fa8a6564e891e656c2
Jin <75513525+Candyzorua@users.noreply.github.com>
2023-01-06 11:17:47 -0800

    Update find_translation_sentence_backend.py

--- find_translation_sentence_backend.py
+++ find_translation_sentence_backend.py
@@ -5,6 +5,7 @@ import flask
 from flask import Flask, request
 from flask_cors import CORS
 
+#initialize Flask object
 app = Flask(__name__)
 CORS(app)
 
@@ -16,8 +17,9 @@ def words():
         received_data = request.get_json()
         message = received_data['data']
         return_data = json.dumps(g.findtranslations(message))
-        print(g.findtranslations(message))
+        #print(g.findtranslations(message))
         return flask.Response(response=return_data, status=201)
-    
+
+#only run the Flask application if this file is executed as a script and not imported
 if __name__ == "__main__":
     app.run("localhost", 6969)
